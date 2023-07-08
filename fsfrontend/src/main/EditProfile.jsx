1eeb43c793a4e42b910ca6c3b23c86f529033e86
manojpethe <manoj.pethe@gmail.com>
2023-07-08 15:44:02 +0530

    editProfile updated

--- fsfrontend/src/main/EditProfile.jsx
+++ fsfrontend/src/main/EditProfile.jsx
@@ -8,18 +8,18 @@ import { InputTextarea } from 'primereact/inputtextarea';
 let basicInfo = { fullName: "Manoj Pethe", height: "6.4", relgion: "Hindu", motherTongue: "marathi", caste: "Bramhin", annualIncome: "", city: "Pune", state: "Maharashtra", country: "India", profileManager: "Manoj Pethe" };
 
 const ShowBasicInfo = () => {
-  return (<div style={{"fontFamily":"verdana"}}>
-    <div><br/>
-    <div className="grid" style={{ "width": "100%" }}>
-    <div className="col-12 lg:col-12 md:col-12">Full Name</div>
-    <div className="col-6 lg:col-6 md:col-6">Height</div>
-    <div className="col-6 lg:col-6 md:col-6">Religion</div>
-    <div className="col-6 lg:col-6 md:col-6">Mother Tongue</div>
-    <div className="col-6 lg:col-6 md:col-6">Annual Income</div>
-    <div className="col-6 lg:col-6 md:col-6">Location</div>
-    <div className="col-6 lg:col-6 md:col-6">Caste</div>
-    <div className="col-6 lg:col-6 md:col-6">Profile Managed by</div>
-    </div>
+  return (<div>
+    <div><br />
+      <div className="grid" style={{ "width": "100%" }}>
+        <div className="col-12 lg:col-12 md:col-12">Full Name</div>
+        <div className="col-6 lg:col-6 md:col-6">Height</div>
+        <div className="col-6 lg:col-6 md:col-6">Religion</div>
+        <div className="col-6 lg:col-6 md:col-6">Mother Tongue</div>
+        <div className="col-6 lg:col-6 md:col-6">Annual Income</div>
+        <div className="col-6 lg:col-6 md:col-6">Location</div>
+        <div className="col-6 lg:col-6 md:col-6">Caste</div>
+        <div className="col-6 lg:col-6 md:col-6">Profile Managed by</div>
+      </div>
     </div>
   </div>)
 }
@@ -27,58 +27,93 @@ const ShowBasicInfo = () => {
 const EditBasicInfo = (props) => {
   return (<div>
     <div className="grid" style={{ "width": "100%" }}>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }} >Full Name</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }} >Full Name</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Gender</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Gender</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Height</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Height</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Religion</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Religion</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Mother Tongue</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Mother Tongue</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Caste</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Caste</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Country</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Country</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>City</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>City</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Annual Income</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Annual Income</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
-      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left","fontFamily":"verdana", }}>Profile Managed by</div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Profile Managed by</div>
       <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
       <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "right" }}></div>
       <div className="col-12 lg:col-8 md:col-8">
-        <Button onClick={()=>{props.setEditBasicInfoToggle(false)}} label='&nbsp;Save&nbsp;' severity='danger' />&nbsp;
-        <Button onClick={()=>{props.setEditBasicInfoToggle(false)}} label='Cancel' severity='secondary' />
+        <Button onClick={() => { props.setEditBasicInfoToggle(false) }} label='&nbsp;Save&nbsp;' severity='danger' />&nbsp;
+        <Button onClick={() => { props.setEditBasicInfoToggle(false) }} label='Cancel' severity='secondary' />
       </div>
     </div>
   </div>
   )
 }
 
+const ShowCriticalInfo = () => {
+  return (<div>
+    <div><br />
+      <div className="grid" style={{ "width": "100%" }}>
+        <div className="col-6 lg:col-6 md:col-6">Birthdate</div>
+        <div className="col-6 lg:col-6 md:col-6">Marital Status</div>
+      </div>
+    </div>
+  </div>)
+}
+
+const EditCriticalInfo = (props) => {
+  return (<div>
+    <div className="grid" style={{ "width": "100%" }}>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }} >Birthdate</div>
+      <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "left", "fontFamily": "verdana", }}>Marital Status</div>
+      <div className="col-12 lg:col-8 md:col-8"><InputText style={{ "width": "100%" }} /></div>
+      <div className="col-12 lg:col-4 md:col-4" style={{ "textAlign": "right" }}></div>
+      <div className="col-12 lg:col-8 md:col-8">
+        <Button onClick={() => { props.toggle(false) }} label='&nbsp;Save&nbsp;' severity='danger' />&nbsp;
+        <Button onClick={() => { props.toggle(false) }} label='Cancel' severity='secondary' />
+      </div>
+    </div>
+  </div>
+  )
+}
+
+
 const EditProfile = () => {
   const [editBasicInfoToggle, setEditBasicInfoToggle] = useState(false);
+  const [editCriticalInfoToggle, setEditCriticalInfoToggle] = useState(false);
 
   return (
     <div className="grid" style={{ "width": "100%" }}>
       <div className="col-12 lg:col-3 md:col-3 "></div>
       <div className="col-12 lg:col-6 md:col-6">
-        <div style={{"fontFamily":"verdana","marginTop":"10px", "marginLeft":"20px","marginBottom":"10px"}}>Edit Profile</div>
-        <div style={{"marginLeft":"20px"}}>
-          <div><div className='text-pink-600' style={{ "float": "left","fontFamily":"verdana", }}>Basic Information</div><div style={{ "float": "right" }}>
-            <Button onClick={()=>{setEditBasicInfoToggle(true)}} label="Edit" severity="danger" text />
+        <Panel header="Edit Profile">
+          {/* <div style={{"fontFamily":"verdana","fontSize":"15px","color":"#363636","marginTop":"10px", "marginLeft":"20px","marginBottom":"10px"}}>Edit Profile</div> */}
+          <div style={{ "marginLeft": "20px","marginBottom":"10px" }}>
+            <div><div className='text-pink-600' style={{ "float": "left" }}>Basic Information</div><div style={{ "float": "right" }}>
+              <Button onClick={() => { setEditBasicInfoToggle(true) }} label="Edit" severity="danger" text />
+            </div>
+            </div>
+            <br />
+            {!editBasicInfoToggle ? <ShowBasicInfo setEditBasicInfoToggle={setEditBasicInfoToggle} /> : <EditBasicInfo setEditBasicInfoToggle={setEditBasicInfoToggle} />}
           </div>
+          <div style={{ "marginLeft": "20px","marginBottom":"10px" }}>
+            <div><div className='text-pink-600' style={{ "float": "left" }}>Critical Information</div><div style={{ "float": "right" }}>
+              <Button onClick={() => { setEditCriticalInfoToggle(true) }} label="Edit" severity="danger" text />
+            </div>
+            </div>
+            <br />
+            {!editCriticalInfoToggle ? <ShowCriticalInfo /> : <EditCriticalInfo toggle={setEditCriticalInfoToggle} />}
           </div>
-          <br />
-          {!editBasicInfoToggle ? <ShowBasicInfo setEditBasicInfoToggle={setEditBasicInfoToggle} /> : <EditBasicInfo setEditBasicInfoToggle={setEditBasicInfoToggle} />}
-        </div>
-        <Panel header="Critical Information">
-          <p className="m-0">
-            birthdate & Marital Status
-          </p>
         </Panel>
-        <Panel header="About Me">
+        {/* <Panel header="About Me">
           <p className="m-0">
             about me, my family, about education, my career
           </p>
@@ -103,7 +138,7 @@ const EditProfile = () => {
           <p className="m-0">
             Art liking, Hobbies, Music, cloathing, movie, sports, cooking, travelled places
           </p>
-        </Panel>
+        </Panel> */}
       </div>
       <div className="col-12 lg:col-3 md:col-3"></div>
     </div>
